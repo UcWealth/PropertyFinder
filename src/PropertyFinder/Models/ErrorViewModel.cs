18406c317228626637f5bce8e67a296644216c7b
ucwealth <ucheokeke16@gmail.com>
2020-05-28 22:32:55 +0100

    added project

---
+++ src/PropertyFinder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PropertyFinder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
