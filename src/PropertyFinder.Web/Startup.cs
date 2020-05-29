c92867b75dab937784550eaf1a412dd775e18ccf
ucwealth <ucheokeke16@gmail.com>
2020-05-29 16:09:03 +0100

    added db context to startup.cs

--- src/PropertyFinder.Web/Startup.cs
+++ src/PropertyFinder.Web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PropertyFinder.Data.DatabaseContexts.AuthenticationDbContext;
 
 namespace PropertyFinder.Web
 {
@@ -23,6 +25,7 @@ namespace PropertyFinder.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
             services.AddControllersWithViews();
         }
 
