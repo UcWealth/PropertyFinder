fc3d25ab9f4dbb5cc31fe83f15a1c38f5bccaaf2
ucwealth <ucheokeke16@gmail.com>
2020-05-29 15:28:53 +0100

    added authentication db context class

--- src/PropertyFinder.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/PropertyFinder.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PropertyFinder.Data.Entities;
+
 namespace PropertyFinder.Data.DatabaseContexts.AuthenticationDbContext
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext>options) : base(options)
+        {
+
+        }
     }
 }
---
+++ src/PropertyFinder.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace PropertyFinder.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
